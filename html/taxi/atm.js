c5f81305353094adce299b4b692b5b75f51a4b39
severingronvick <47294371+severingronvick@users.noreply.github.com>
2019-06-02 15:04:00 +0300

    начало решения задач

---
+++ html/taxi/atm.js
@@ -0,0 +1,7 @@
+let limits = {'100': 10, '200': 30, '500': 40}, nominals = [500, 200, 100];
+
+function atm(sum) {
+if (sum % 100 != 0) return 'Error: incorrect sum';
+let needSum = sum, nominal = nominals[0];
+Math.floor()
+}
